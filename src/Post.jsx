1238f0163f6fcc6729a1bbd1914fbb38873151d5
Rishabh <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2023-07-09 10:06:07 +0530

    updated

--- src/Post.jsx
+++ src/Post.jsx
@@ -9,7 +9,7 @@ function Post({_id,title,summary,content,cover,createdAt,author}) {
     <div className="mt-3 mx-auto w-2/3 md:w-full border-2 grid grid-cols-1 items-center justify-center md:grid-cols-2 gap-4 hover:shadow-xl p-2 rounded delay-150 ease-in-out">
       <div className="m-0 md:m-3">
       <img src={'https://mern-blog-app-server-gold.vercel.app/'+cover} alt="Rice packets"
-      className="w-full md:p-5 max-w-full "/>
+      className="md:p-5 w-40 object-cover"/>
       </div>
       <div className=" flex items-center md:items-start flex-col gap-3">
       <span className=" flex gap-2 text-sm">
